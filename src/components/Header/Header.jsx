9300b5fe2d58159c3cab49d9c1b35857554ae9af
alejandro-programador2 <programador2@booksandbooksdigital.com.co>
2022-11-16 16:36:40 -0500

    Creado componente Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,81 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import css from "./Header.module.css";
+
 const Header = () => {
-  return <div>Header</div>;
+  const [active, setActive] = useState(false);
+
+  const onOpenMenu = () => setActive(!active);
+
+  return (
+    <header className={css["c-header"]}>
+      <button className={css["c-header__icon"]}>
+        <img
+          src="assets/images/icon-home.png"
+          alt="Herradura con una figura en forma de casa"
+        />
+      </button>
+
+      <div className={css["c-header__content"]}>
+        <img
+          className={css["c-header__logo"]}
+          src="https://virtual.ucundinamarca.edu.co/red/baseUNAD/maquetaRED/assets/img_contenido/logo.png"
+          alt=""
+        />
+
+        <nav className={css["c-nav"]}>
+          <ul role="list" className={css["c-nav__list"]}>
+            <li>
+              <Link to="/">Introducción</Link>
+            </li>
+
+            <li>
+              <Link to="/">Resultado esperado</Link>
+            </li>
+
+            <li>
+              <Link to="/">Contenido</Link>
+            </li>
+
+            <li>
+              <button onClick={onOpenMenu} aria-expanded={active}>
+                ...
+              </button>
+              <ul
+                role="list"
+                className={`${css["c-nav__submenu"]} ${
+                  active && css["is-nav__submenu--active"]
+                }`}
+              >
+                <li>
+                  <Link to="/">Actividades de aprendizaje</Link>
+                </li>
+
+                <li>
+                  <Link to="/">Como se evaluará este curso</Link>
+                </li>
+
+                <li>
+                  <Link to="/">Ref. bibliográfica</Link>
+                </li>
+
+                <li>
+                  <Link to="/">Créditos</Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <button className={css["c-header__icon"]}>
+        <img
+          src="assets/images/icon-horse.png"
+          alt="Herradura con una figura de un caballo"
+        />
+      </button>
+    </header>
+  );
 };
 
 export { Header };
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,7 +1,18 @@
+import { useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 
+import { Header } from "@components";
+import { PaginationProvider } from "@context/PaginationContext";
+
 const Layout = ({ children }) => {
-  return <div>{children}</div>;
+  const location = useLocation();
+
+  return (
+    <PaginationProvider>
+      {location.pathname !== "/" && <Header />}
+      <main tabIndex="-1">{children}</main>
+    </PaginationProvider>
+  );
 };
 
 Layout.propTypes = {
