66c3b3d2728734f68fc03444c4956d4960233db6
alejandro-programador2 <programador2@booksandbooksdigital.com.co>
2022-11-19 17:59:26 -0500

    Eliminado submodule UI-Components

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,6 +1,8 @@
 import { useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 
+import { Col } from "UI-components";
+
 import { Header } from "@components";
 import { PaginationProvider } from "@context/PaginationContext";
 
@@ -10,7 +12,9 @@ const Layout = ({ children }) => {
   return (
     <PaginationProvider>
       {location.pathname !== "/" && <Header />}
-      <main tabIndex="-1">{children}</main>
+      <main tabIndex="-1">
+        {children} <Col />
+      </main>
     </PaginationProvider>
   );
 };
